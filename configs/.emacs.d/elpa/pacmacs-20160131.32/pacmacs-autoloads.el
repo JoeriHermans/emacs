49786638cfc22ddcd9b239fe0a58623a2899a753
Joeri Hermans <joeri@joerihermans.com>
2016-06-21 11:17:33 +0200

    Add snippets.

---
+++ configs/.emacs.d/elpa/pacmacs-20160131.32/pacmacs-autoloads.el
@@ -0,0 +1,33 @@
+;;; pacmacs-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "pacmacs" "pacmacs.el" (22340 7953 272841 47000))
+;;; Generated autoloads from pacmacs.el
+
+(autoload 'pacmacs-start "pacmacs" "\
+
+
+\(fn)" t nil)
+
+(autoload 'pacmacs-score "pacmacs" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pacmacs-anim.el" "pacmacs-board.el" "pacmacs-pkg.el"
+;;;;;;  "pacmacs-render.el" "pacmacs-score.el" "pacmacs-utils.el"
+;;;;;;  "pacmacs-vector.el" "pacmacs-walls.el") (22340 7956 263678
+;;;;;;  29000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pacmacs-autoloads.el ends here
